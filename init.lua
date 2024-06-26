d7988367d048e3ab22bf53fc381be31520ee6c69
Thanmatt <belphegorous@gmail.com>
2024-06-26 11:30:58 +0800

    First commit

---
+++ init.lua
@@ -0,0 +1,3 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- use `vim.keymap.set` instead
+local map = LazyVim.safe_keymap_set
+
+map("n", "gT", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "gt", "<cmd>bnext<cr>", { desc = "Next Buffer" })
